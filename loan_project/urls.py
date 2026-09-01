e500ec9305eb18775b9bbd5dcd873a2ab53c234e
MuzakaNajih25 <muzakanajih058@gmail.com>
2026-09-01 08:18:10 +0700

    update urlspy in loan project

--- loan_project/urls.py
+++ loan_project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loancalc.urls')),
     path('bukti-survei/', include('buktisurvei.urls')),
+    path("wablast/", include("wablast.urls")),
 ]
 
 if settings.DEBUG:
