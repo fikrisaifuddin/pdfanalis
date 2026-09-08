23c09c2bec5a1ced13095c571a235c75eaa6e1fc
fikrisabili01 <165111469+fikrisabili01@users.noreply.github.com>
2026-09-08 09:15:52 +0700

    index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 # Tambahkan root directory ke sys.path untuk Vercel Serverless Function
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from loan_project.wsgi import app
